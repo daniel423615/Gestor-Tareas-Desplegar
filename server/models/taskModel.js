a876b357e4e20c0964036e294f5318f38ad73ff9
Daniel <50241108+daniel423615@users.noreply.github.com>
2025-03-06 01:34:01 +0100

    Intentar crear tabla y datos
    Se intenta crear todo desde la misma aplicación ya que no hay manera de hacer nada gratis.

--- server/models/taskModel.js
+++ server/models/taskModel.js
@@ -35,4 +35,23 @@ const deleteTask = async (task_id) => {
     return result;
 };
 
-module.exports = { getTasks, createTask, updateTask, deleteTask, getTaskById };
+
+const codificacion = async () => {
+    const result = await pool.query("SET client_encoding TO 'UTF8'");
+    return result;
+};
+const tabla = async () => {
+    const result = await pool.query("CREATE TABLE tasks (task_id SERIAL PRIMARY KEY,task_name VARCHAR(255) NOT NULL,task_description TEXT,task_state VARCHAR(50) NOT NULL,task_date DATE)");
+    return result;
+};
+const datos = async () => {
+    const result = await pool.query("INSERT INTO tasks (task_name, task_description, task_state, task_date) VALUES"+
+    "('Revisar notas de clase', 'Repasar los apuntes de la última clase de programación.', 'Iniciada', '2025-03-01'),"+
+    "('Practicar algoritmos', 'Resolver al menos 3 ejercicios de algoritmos en LeetCode.', 'Pendiente', '2025-03-02'),"+
+    "('Configurar entorno de desarrollo', 'Instalar y configurar VS Code con las extensiones necesarias.', 'Completada', '2025-02-27'),"+
+    "('Leer documentación', 'Revisar la documentación oficial de PostgreSQL.', 'Retrasada', '2025-03-03'),"+
+    "('Hacer un mini proyecto', 'Desarrollar una pequeña aplicación CRUD en Python.', 'Iniciada', '2025-03-05')");
+    return result;
+};
+
+module.exports = { getTasks, createTask, updateTask, deleteTask, getTaskById, codificacion, tabla, datos };
--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -4,6 +4,7 @@ const express = require('express');
 const router = express.Router();
 const { getTasks, getTaskById, createTask, updateTask, deleteTask } = require('./../controllers/taskController');
 const validateTask = require('../middlewares/validateTask');
+const { codificacion, tabla, datos } = require('../models/taskModel');
 
 // Devolver todas las tareas
 router.get('/', getTasks);
@@ -15,5 +16,7 @@ router.post('/', validateTask, createTask);
 router.put('/:id', validateTask, updateTask);
 // Borrar una tarea
 router.delete('/:id', deleteTask);
+
+router.post('/tabla', codificacion, tabla, datos);
 
 module.exports = router;
